df95fbb88d7f34b737766c243b21db551295a3f9
S-Patel05 <sohampatel8609@gmail.com>
2026-01-09 14:40:07 +0530

    auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
-import { clerkMiddleware } from '@clerk/express'
+import { clerkMiddleware } from '@clerk/express';
+import { serve } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 
@@ -8,7 +10,10 @@ import { connectDB } from "./config/db.js";
 const app = express();
 const __dirname = path.resolve();
 
+app.use(express.json())
 app.use(clerkMiddleware())  //adds auth object under the request
+
+app.use("/api/inngest", serve({ client: inngest, functions}));
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
@@ -22,9 +27,11 @@ if (ENV.NODE_ENV === "production") {
   });
 }
 
-const PORT = ENV.PORT || 3000;
+const startServer = async() => {
+  await connectDB();
+  app.listen(ENV.PORT, () => {
+    console.log("server is up and running")
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
-  connectDB();
-});
+startServer()
