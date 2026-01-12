e1729ab6d660ae71272c63dc805ac4ab5dc39ed8
S-Patel05 <sohampatel8609@gmail.com>
2026-01-12 16:56:02 +0530

    user routes and controllers added

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import { functions, inngest } from "./config/inngest.js";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import adminRoutes from "./routes/admin.route.js";
+import userRoutes from "./routes/user.route.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -15,7 +16,9 @@ app.use(clerkMiddleware())  //adds auth object under the request
 
 app.use("/api/inngest", serve({ client: inngest, functions}));
 
-app.use("/api/admin", adminRoutes)
+app.use("/api/admin", adminRoutes);
+
+app.use("/api/users", userRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
