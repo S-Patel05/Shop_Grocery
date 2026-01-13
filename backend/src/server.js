b84ae1dea983c4977abdb27f6ffc241dff3d77b0
S-Patel05 <sohampatel8609@gmail.com>
2026-01-13 11:50:37 +0530

    cart routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -10,12 +10,14 @@ import userRoutes from "./routes/user.route.js";
 import orderRoutes from "./routes/order.route.js";
 import reviewRoutes from "./routes/review.route.js";
 import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json())
 app.use(clerkMiddleware())  //adds auth object under the request
+app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
 
 app.use("/api/inngest", serve({ client: inngest, functions}));
 
@@ -28,6 +30,8 @@ app.use("/api/orders", orderRoutes);
 app.use("/api/reviews", reviewRoutes);
 
 app.use("/api/products", productRoutes);
+
+app.use("/api/carts", cartRoutes);
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "success" });
