4d75b7f529539477aab994275539dfcd28ecc2f7
S-Patel05 <sohampatel8609@gmail.com>
2026-01-20 22:49:01 +0530

    fixing the error

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,9 +35,9 @@ const __dirname = path.resolve();
 //   paymentRoutes
 // );
 
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 app.use(express.json());
 app.use(clerkMiddleware()); // adds auth object under the req => req.auth
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true })); // credentials: true allows the browser to send the cookies to the server with the request
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
